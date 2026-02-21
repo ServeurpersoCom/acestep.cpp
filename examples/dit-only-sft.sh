63b1ce292f9909795b3e96fb1d38fe81c9c8fc6b
Pascal <admin@serveurperso.com>
2026-02-21 01:48:46 +0100

    scripts

--- examples/dit-only-sft.sh
+++ examples/dit-only-sft.sh
@@ -6,4 +6,4 @@ set -eu
     --request dit-only-sft.json \
     --text-encoder ../models/Qwen3-Embedding-0.6B-Q8_0.gguf \
     --dit ../models/acestep-v15-sft-Q8_0.gguf \
-    --vae ../models/vae-BF16.gguf \
+    --vae ../models/vae-BF16.gguf
--- examples/full-sft.sh
+++ examples/full-sft.sh
@@ -10,4 +10,4 @@ set -eu
     --request full-sft0.json \
     --text-encoder ../models/Qwen3-Embedding-0.6B-Q8_0.gguf \
     --dit ../models/acestep-v15-sft-Q8_0.gguf \
-    --vae ../models/vae-BF16.gguf \
+    --vae ../models/vae-BF16.gguf
